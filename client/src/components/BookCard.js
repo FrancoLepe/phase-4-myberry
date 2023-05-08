eefafbc51dc77b262256aa0b8d7388a74f2b9564
Scott <scotthenry1@gmail.com>
2023-05-08 13:51:43 -0400

    Gave book cover images a set height

--- client/src/components/BookCard.js
+++ client/src/components/BookCard.js
@@ -6,7 +6,7 @@ function BookCard({book}){
         <div>Title:{book.title}</div>
         <div>Author: {book.author}</div>
         <div>Genre: {book.genre}</div> 
-        <div><img src={book.image} alt= {book.title}/> </div>
+        <div><img src={book.image} alt= {book.title} height="300px" /> </div>
         <br />
     </div>
     )
