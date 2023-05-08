21d855684f83503a74d6fbbe2f4cd1562f59a426
Franco Lepe <Francolepe169@gmail.com>
2023-05-08 11:10:32 -0400

    tailwind install

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,23 @@
-import { Route, Switch } from "react-router";
-import Home from "./components/Home";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <div>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-      </Switch>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
